dd294ed448453ea75c4e6de926bd07fb2d67a506
Saturn745 <90934664+Saturn745@users.noreply.github.com>
2023-09-10 14:30:06 -0700

    Github CI to build and publish to Modrinth

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
-import net.minecrell.pluginyml.paper.PaperPluginDescription
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.0"
     id("net.minecrell.plugin-yml.paper") version "0.6.0"
+    id("com.modrinth.minotaur") version "2.8.2"
     application
 }
-
+val buildNum = System.getenv("GITHUB_RUN_NUMBER") ?: "SNAPSHOT"
 group = "xyz.galaxyy.mclua"
-version = "1.0-SNAPSHOT"
+version = "1.20.1-$buildNum"
 
 repositories {
     mavenCentral()
@@ -41,6 +41,18 @@ paper {
     // Generate paper-libraries.json from `library` and `paperLibrary` in `dependencies`
     generateLibrariesJson = true
 }
+
+modrinth {
+    token.set(System.getenv("MODRINTH_TOKEN")) // Remember to have the MODRINTH_TOKEN environment variable set or else this will fail - just make sure it stays private!
+    projectId.set("mclua") // This can be the project ID or the slug. Either will work!
+    versionNumber.set(version.toString()) // You don't need to set this manually. Will fail if Modrinth has this version already
+    versionType.set("beta") // This is the default -- can also be `beta` or `alpha`
+    uploadFile.set(tasks.jar.get())
+    gameVersions.addAll("1.20.1")
+    loaders.addAll("paper", "purpur")
+    changelog.set(System.getenv("GIT_COMMIT_MESSAGE"))
+}
+
 
 tasks.test {
     useJUnitPlatform()
