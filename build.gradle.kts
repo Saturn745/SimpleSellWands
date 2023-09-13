afd4b53edd3e780ccec17d1d0f4c41d376182307
Saturn745 <90934664+Saturn745@users.noreply.github.com>
2023-09-12 18:38:14 -0700

    Let's all pretend like this was never here. [ci-skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,7 +66,7 @@ hangarPublish {
         channel.set("Beta")
         changelog.set(System.getenv("GIT_COMMIT_MESSAGE")) // optional
 
-        apiKey.set("03259ae1-dde1-4334-ae63-f35cfb93dba1.968fa354-fe17-4b0a-8f7a-ad589c77075d")
+        apiKey.set(System.getenv("HANGAR_API_KEY"))
 
         platforms {
             register(Platforms.PAPER) {
