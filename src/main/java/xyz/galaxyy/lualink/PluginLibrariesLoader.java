65ce0ab517260daa099c6ef4522619c4e081def2
Grabsky <44530932+Grabsky@users.noreply.github.com>
2023-09-21 00:31:52 +0200

    user-specified libraries (#6)

--- src/main/java/xyz/galaxyy/lualink/PluginLibrariesLoader.java
+++ src/main/java/xyz/galaxyy/lualink/PluginLibrariesLoader.java
@@ -1,6 +1,7 @@
 package xyz.galaxyy.lualink;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
 import io.papermc.paper.plugin.loader.PluginLoader;
 import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
@@ -9,40 +10,70 @@ import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
-public class PluginLibrariesLoader implements PluginLoader {
+@SuppressWarnings("UnstableApiUsage")
+public final class PluginLibrariesLoader implements PluginLoader {
+
+    private final Gson gson = new GsonBuilder().setLenient().create();
+
     @Override
-    public void classloader(@NotNull PluginClasspathBuilder classpathBuilder) {
-        MavenLibraryResolver resolver = new MavenLibraryResolver();
-        PluginLibraries pluginLibraries = load();
-        pluginLibraries.asDependencies().forEach(resolver::addDependency);
-        pluginLibraries.asRepositories().forEach(resolver::addRepository);
-        classpathBuilder.addLibrary(resolver);
-    }
+    public void classloader(final @NotNull PluginClasspathBuilder classpathBuilder) {
+        // Loading internal libraries.
+        try (final InputStream stream = this.getClass().getClassLoader().getResourceAsStream("paper-libraries.json")) {
+            // Extracting file contents to new MavenLibraryResolver instance.
+            final MavenLibraryResolver iLibraries = gson.fromJson(new InputStreamReader(stream, StandardCharsets.UTF_8), PluginLibraries.class)
+                    .toMavenLibraryResolver();
+            // Adding library(-ies) to the PluginClasspathBuilder.
+            classpathBuilder.addLibrary(iLibraries);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
 
-    public PluginLibraries load() {
-        try (var in = getClass().getResourceAsStream("/paper-libraries.json")) {
-            return new Gson().fromJson(new InputStreamReader(in, StandardCharsets.UTF_8), PluginLibraries.class);
-        } catch (IOException e) {
+        final File eLibrariesFile = new File(classpathBuilder.getContext().getDataDirectory().toFile(), "libraries.json");
+        // Copying default file to "plugins/LuaLink/..."
+        if (!eLibrariesFile.exists()) {
+            eLibrariesFile.getParentFile().mkdirs();
+            try (final InputStream stream  = this.getClass().getClassLoader().getResourceAsStream("libraries.json")) {
+                Files.copy(stream, eLibrariesFile.toPath());
+            } catch (final IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        // Loading external (user-specified) libraries.
+        try {
+            // Extracting file contents to new MavenLibraryResolver instance.
+            final MavenLibraryResolver eDependencies = gson.fromJson(new InputStreamReader(new FileInputStream(eLibrariesFile), StandardCharsets.UTF_8), PluginLibraries.class)
+                    .toMavenLibraryResolver();
+            // Adding library(-ies) to the PluginClasspathBuilder.
+            classpathBuilder.addLibrary(eDependencies);
+        } catch (final FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 
     private record PluginLibraries(Map<String, String> repositories, List<String> dependencies) {
-        public Stream<Dependency> asDependencies() {
-            return dependencies.stream()
-                    .map(d -> new Dependency(new DefaultArtifact(d), null));
-        }
 
-        public Stream<RemoteRepository> asRepositories() {
-            return repositories.entrySet().stream()
-                    .map(e -> new RemoteRepository.Builder(e.getKey(), "default", e.getValue()).build());
+        public MavenLibraryResolver toMavenLibraryResolver() {
+            final MavenLibraryResolver resolver = new MavenLibraryResolver();
+            // ...
+            repositories.entrySet().stream()
+                    .map(entry -> new RemoteRepository.Builder(entry.getKey(), "default", entry.getValue()).build())
+                    .forEach(resolver::addRepository);
+            dependencies.stream()
+                    .map(value -> new Dependency(new DefaultArtifact(value), null))
+                    .forEach(resolver::addDependency);
+            // ...
+            return resolver;
         }
     }
 }
